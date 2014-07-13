e062c1fb51b3828e652fbc7a0fce14ccf43400e1
Raveendra Bhat <me@raveendra.in>
2014-07-13 18:46:56 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function(request, response) {
+	response.writeHead(200, {
+		"ContentType" : "text/plain"
+	});
+	response.write('Hello World');
+	response.end();
+}).listen(8080);
